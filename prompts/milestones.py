8eab9d61b7fea6c050b18730db488fa816b7262f
Blake Martin <56556280+bamartin1618@users.noreply.github.com>
2022-09-28 21:33:58 -0500

    fix issue with exception statement

--- prompts/milestones.py
+++ prompts/milestones.py
@@ -17,7 +17,7 @@ def milestones_model(input):
             presence_penalty = 0
         )
         return response["choices"][0]["text"]
-    except:
+    except Exception as e:
         return "milestones error" # placeholder for now
 
 """
