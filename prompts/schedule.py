2a7a002ea9ea55d3fa1e59908541dcee556d498d
Blake Martin <56556280+bamartin1618@users.noreply.github.com>
2022-09-28 21:34:38 -0500

    Update schedule.py

--- prompts/schedule.py
+++ prompts/schedule.py
@@ -17,7 +17,7 @@ def schedule_model(input):
             presence_penalty = 0
         )
         return response["choices"][0]["text"]
-    except:
+    except Exception as e:
         return "schedule error" # placeholder for now
         
 """
