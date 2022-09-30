6d10664f41ecbe2dc58d29038993500897c50efb
VamseeC21 <112673425+VamseeC21@users.noreply.github.com>
2022-09-30 19:37:17 -0400

    push bot code to repo

---
+++ Discord_Bot/responses.py
@@ -0,0 +1,11 @@
+
+from email import message
+
+
+def handle_response(message) -> str:
+    
+    if message == 'hello':
+        return 'Hey there!'
+    
+    if message == '$score solution_statement':
+        return 'This function will return a score and a reason once the code is done!'
