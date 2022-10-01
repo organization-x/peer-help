029552e9cccdf61317411f719a849ffedbfe3cc9
ian <104332334+iankengott@users.noreply.github.com>
2022-10-01 11:46:12 -0400

    Update model.py
    added backend to ai conections and listed un added things

--- model.py
+++ model.py
@@ -5,13 +5,20 @@ from prompts.tech_stack import tech_stack_model
 from prompts.milestones import milestones_model
 from prompts.schedule import schedule_model
 from prompts.happy_path import happy_path_model
+from notion_extraction import extract_product_spec_text
+from notion_extraction import parse_product_spec_text
+from notion_extraction import page_id
 
+#from text.peer import notion_token
+
+# unused labels = ['', '', '', 'Success Criteria', 'Success Metrics', '', '', '', '']
+    
 # until the backend is implemented
 
-print(problem_model("placeholder"))
-print(solution_model("placeholder"))
-print(target_users_model("placeholder"))
-print(tech_stack_model("placeholder"))
-print(milestones_model("placeholder"))
-print(schedule_model("placeholder"))
-print(happy_path_model("placeholder"))
+print(problem_model(parse_product_spec_text(extract_product_spec_text(page_id))['Problem Statement']))
+print(solution_model(parse_product_spec_text(extract_product_spec_text(page_id))['Solution Statement']))
+print(target_users_model(parse_product_spec_text(extract_product_spec_text(page_id))['Who Has This Problem?']))
+print(tech_stack_model(parse_product_spec_text(extract_product_spec_text(page_id))['Tech Stack']))
+print(milestones_model(parse_product_spec_text(extract_product_spec_text(page_id))['Milestones']))
+print(schedule_model(parse_product_spec_text(extract_product_spec_text(page_id))['Schedule of Deliverables']))
+print(happy_path_model(parse_product_spec_text(extract_product_spec_text(page_id))['Happy Path']))
