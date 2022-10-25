ca0934a779608a5e22acb1517076be3d5ed4df1d
Blake Martin <56556280+bamartin1618@users.noreply.github.com>
2022-10-25 14:20:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from model import main
  
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
  
 @bot.command()
