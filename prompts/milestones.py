d54c9ae59be91a422d213a6fc0f06ef9a8588676
Blake Martin <56556280+bamartin1618@users.noreply.github.com>
2022-12-09 00:28:02 -0600

    Update milestones.py

--- prompts/milestones.py
+++ prompts/milestones.py
@@ -16,7 +16,7 @@ def milestones_model(string):
     """
     try:
         response = openai.Completion.create(
-            model = "text-davinci-002",
+            model = "text-davinci-003",
             prompt = f"The following paragraph is the milestones section of a product specification. Evaluate the milestones and give specific feedback on what can be improved.\n\n\n{string}\n\n\nFEEDBACK:",
             temperature = 1,
             max_tokens = 512,
